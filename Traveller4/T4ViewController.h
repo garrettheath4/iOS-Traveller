c49427e1a4a76c8da9d507ac157e7f857483dfae
Alex Baca <Alex@Alex-Bacas-MacBook-Pro.local>
2012-05-16 19:26:52 -0400

    Added Map View, Added user location service

--- Traveller4/T4ViewController.h
+++ Traveller4/T4ViewController.h
@@ -7,7 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+#import <CoreLocation/CoreLocation.h>
 
 @interface T4ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
 
 @end
