d3f11e90dcbeec784de31f5f49f4ef4d6451b254
Alex Baca <Alex@Alex-Bacas-MacBook-Pro.local>
2012-05-15 22:01:10 -0400

    Second View Renamed

---
+++ Traveller/TTransitViewController.m
@@ -0,0 +1,34 @@
+//
+//  TTransitViewController.m
+//  Traveller
+//
+//  Created by Garrett Koller on 5/14/12.
+//  Copyright (c) 2012 Washington and Lee University. All rights reserved.
+//
+
+#import "TTransitViewController.h"
+
+@interface TTransitViewController ()
+
+@end
+
+@implementation TTransitViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
