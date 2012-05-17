74b36732fd4826255458536f097953d666de409b
Alex Baca <Alex@Alex-Bacas-MacBook-Pro.local>
2012-05-16 21:09:43 -0400

    Filled the Info View with buttons and Text

---
+++ Traveller5/T5InfoViewController.m
@@ -0,0 +1,44 @@
+//
+//  T5InfoViewController.m
+//  Traveller5
+//
+//  Created by Alex Baca on 5/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "T5InfoViewController.h"
+
+@interface T5InfoViewController ()
+
+@end
+
+@implementation T5InfoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
