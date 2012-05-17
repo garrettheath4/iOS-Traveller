70bc8b6bd91bad9e14931d1fbb1ec7b9be869137
Alex Baca <Alex@Alex-Bacas-MacBook-Pro.local>
2012-05-17 18:30:42 -0400

    Added Settings view Coordinator

---
+++ Traveller5/T5SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  T5SettingsViewController.h
+//  Traveller5
+//
+//  Created by Alex Baca on 5/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface T5SettingsViewController : UIViewController
+
+@end
---
+++ Traveller5/T5SettingsViewController.m
@@ -0,0 +1,44 @@
+//
+//  T5SettingsViewController.m
+//  Traveller5
+//
+//  Created by Alex Baca on 5/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "T5SettingsViewController.h"
+
+@interface T5SettingsViewController ()
+
+@end
+
+@implementation T5SettingsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
