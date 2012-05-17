8ab97da7351c56301e8ee49df266688ab8547890
Garrett Heath Koller <kollerg14@mail.wlu.edu>
2012-05-17 19:31:42 -0400

    Added testing suite

---
+++ TestTraveller5/TestTraveller5.h
@@ -0,0 +1,15 @@
+//
+//  TestTraveller5.h
+//  TestTraveller5
+//
+//  Created by Garrett Koller on 5/17/12.
+//  Copyright (c) 2012 Washington and Lee University. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+#import "T5GPSquery.h"
+
+@interface TestTraveller5 : SenTestCase
+
+@end
