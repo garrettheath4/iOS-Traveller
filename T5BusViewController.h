09f65af02947fc49dfb74193ba4713ee3768f057
Alex Baca <Alex@Alex-Bacas-MacBook-Pro.local>
2012-05-18 18:11:13 -0400

    Started the Bus View

---
+++ T5BusViewController.h
@@ -0,0 +1,13 @@
+//
+//  T5BusViewController.h
+//  Traveller5
+//
+//  Created by Alex Baca on 5/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface T5BusViewController : UIViewController
+
+@end
